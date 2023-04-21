37a17d051a2c7c262f7b2089e5a092a3a43df113
Lee Campbell <121746971+leevcampbell@users.noreply.github.com>
2023-04-21 11:43:48 -0400

    Lee push after zina completed

--- lib/classes/sendemail.py
+++ lib/classes/sendemail.py
@@ -11,13 +11,13 @@ match_users = User.query_for_match()
 def send_email(email_sender, email_password, match_users, current_user):
     body = f"""
     Hey there! You've both matched each other's preferences. Here are your email addresses:
-    User 1: {current_user.email}
-    User 2: {match_users.email}
+    User 1: {current_user}
+    User 2: {match_users}
     Happy matching!
     """
     em = EmailMessage()
     em['From'] = email_sender
-    em['To'] = [current_user.email, match_users.email]
+    em['To'] = [current_user, match_users]
     em['Subject'] = "You've got a match!"
     em.set_content(body)
     context = ssl.create_default_context()
@@ -25,7 +25,7 @@ def send_email(email_sender, email_password, match_users, current_user):
         smtp.login(email_sender, email_password)
         smtp.send_message(em)
 
-    send_email('pythontestingphase3@gmail.com', "ekzewlnqqveldcri", f"{match_users.email}", f'{current_user.email}')
+
 
 
 
